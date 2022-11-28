15001f28991abb96aa472dee3b7417f9caf11b28
beba2123 <amanonymous578@gmail.com>
2022-11-28 04:39:05 -0800

    my 3rd task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const argLen = process.argv.length;
 
-if (argLen === 3) {
-  console.log('Argument found');
-} else if (argLen > 3) {
+if (process.argv.length > 3) {
   console.log('Arguments found');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('No argument');
 }
